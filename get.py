4128d66dee445c9dc9f9d092bf6091a64cedd249
Sascha <sascha.maisel@ds.mhie.com>
2018-10-15 19:52:56 +0200

    refactoring & fixing wrong password ('/' vs '\')

--- get.py
+++ get.py
@@ -1,9 +1,12 @@
 import wget
 from netCDF4 import Dataset
 
+username = 'simone.vogel%40student.uni-tuebingen.de' 
+password = '%2FDenker94'
 fileName = 'chess_pet_wwg_196101.nc'
-protectedURL = 'https://password:test:user:test@catalogue.ceh.ac.uk/datastore/eidchub/8baf805d-39ce-4dac-b224-c926ada353b7/'
-wget.download(protectedURL + 'pet/' + fileName)
+protectedURL = 'https://' + username + ':' + password + '@catalogue.ceh.ac.uk/datastore/eidchub/8baf805d-39ce-4dac-b224-c926ada353b7/pet/' + fileName
+print(protectedURL)
+wget.download(protectedURL)
 
 root = Dataset("chess_pet_wwg_196101.nc", "w", format="NETCDF4")
 print(root.data_model)

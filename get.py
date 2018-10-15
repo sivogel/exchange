113951a0574224571edab9a302c1018a7bc0b170
Sascha <sascha.maisel@ds.mhie.com>
2018-10-15 18:33:17 +0200

    perform requests

--- get.py
+++ get.py
@@ -1,3 +1,10 @@
+import requests
+
 payload = {'username': 'Simone.Vogel@student.uni-tuebingen.de', 'password': '/Denker94'}
 loginURL = 'https://catalogue.ceh.ac.uk/sso/login?success=%2Fdatastore%2Feidchub%2F8baf805d-39ce-4dac-b224-c926ada353b7%2F'
 protectedURL = 'https://catalogue.ceh.ac.uk/datastore/eidchub/8baf805d-39ce-4dac-b224-c926ada353b7/'
+
+with requests.Session() as session:
+  req = session.post(loginURL, data=payload)
+  content = session.get(protectedURL + 'checksums.hash')  
+  print(content.text)

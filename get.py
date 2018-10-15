8c21e6a0cf8affcf91c7c71d07d418e7cfc79d30
Sascha <sascha.maisel@ds.mhie.com>
2018-10-15 19:37:59 +0200

    use wget, which seems vastly superior performance-wise

--- get.py
+++ get.py
@@ -1,10 +1,12 @@
-import requests
+import wget
+from netCDF4 import Dataset
 
-payload = {'username': 'Simone.Vogel@student.uni-tuebingen.de', 'password': '/Denker94'}
-loginURL = 'https://catalogue.ceh.ac.uk/sso/login?success=%2Fdatastore%2Feidchub%2F8baf805d-39ce-4dac-b224-c926ada353b7%2F'
-protectedURL = 'https://catalogue.ceh.ac.uk/datastore/eidchub/8baf805d-39ce-4dac-b224-c926ada353b7/'
+fileName = 'chess_pet_wwg_196101.nc'
+protectedURL = 'https://password:test:user:test@catalogue.ceh.ac.uk/datastore/eidchub/8baf805d-39ce-4dac-b224-c926ada353b7/'
+wget.download(protectedURL + 'pet/' + fileName)
 
-with requests.Session() as session:
-  req = session.post(loginURL, data=payload)
-  content = session.get(protectedURL + 'checksums.hash')  
-  print(content.text)
+root = Dataset("chess_pet_wwg_196101.nc", "w", format="NETCDF4")
+print(root.data_model)
+
+#do stuff here
+root.close()
